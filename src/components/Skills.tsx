9cb428894f4294ae71d1ef91d44b56f5c4a9d182
Vraj08 <88830015+Vraj08@users.noreply.github.com>
2025-05-14 22:32:07 -0700

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -183,27 +183,6 @@ const technologies: Technologies = {
   ],
 };
 
-const fadeIn = (direction: string, type: string, delay: number, duration: number) => {
-  return {
-    hidden: {
-      x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
-      y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
-      opacity: 0
-    },
-    show: {
-      x: 0,
-      y: 0,
-      opacity: 1,
-      transition: {
-        type: type,
-        delay: delay,
-        duration: duration,
-        ease: "easeOut"
-      }
-    }
-  };
-};
-
 const textVariant = () => {
   return {
     hidden: {
